0b90072ada22747e27b7a9da409db8bd80b6059f
surajrai-code <surajraieng>
2023-08-11 12:09:15 +0530

    change

--- app.js
+++ app.js
@@ -20,25 +20,3 @@ app.use(erroring.error)
 app.listen(3000,() => {
     console.log('Request has been made');
 } )
-const express = require('express');
-const bodyParser = require('body-parser');
-const path=require('path');
-const adminRoutes=require('./routes/admin');
-const contacting=require('./routes/contact');
-const shoproutes=require('./routes/shop');
-const erroring=require('./controller/error');
-
-
-
-app.use('/shop',shoproutes); 
-app.use('/admin',adminRoutes);
-app.use(contacting);
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));//iske vajah se hi hum css files ko html files mai access kar pa rhe hain varna ni kar paate html file css ko access ni kar pata, ye koi bhi request lega jo tries to find some files like .css .js aur usko public ko forward kar dega.
-
-app.use(erroring.error)
-
-app.listen(3000,() => {
-    console.log('Request has been made');
-} )
